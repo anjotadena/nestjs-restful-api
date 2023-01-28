1c658d2d645ffc00992bf07f5661476f8485c2e5
Anjo Tadena <tadena.anjo@gmail.com>
2023-01-28 14:54:11 +0800

    Set api endpoint prefix globally

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Set endpoint prefix
+  app.setGlobalPrefix('api');
+
   await app.listen(3000);
 }
 bootstrap();
